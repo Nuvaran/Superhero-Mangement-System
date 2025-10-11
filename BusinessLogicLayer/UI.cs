a26b8c3e7a6c78df737cf30bdf6e81b5020527f6
Nuvaran <rng.nuvi777@gmail.com>
2025-10-11 14:04:37 +0200

    "First Commit"

---
+++ BusinessLogicLayer/UI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superhero_Mangement_System.BusinessLogicLayer
+{
+    internal class UI
+    {
+    }
+}
